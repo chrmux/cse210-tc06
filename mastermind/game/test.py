204a8088426fca5164900bd51d09a1e98c1a4b41
Jerry Marollano <70122455+chrmux@users.noreply.github.com>
2021-10-05 13:44:45 +0800

    Create test.py

---
+++ mastermind/game/test.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+class Director:
+    def test_get_code(self):
+        pass
+
+    def test_start_game(self):
+        pass
+
+
+
+class Code():
+    def test_generate_code():
+        pass
