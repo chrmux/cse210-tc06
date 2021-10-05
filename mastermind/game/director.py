16a6179ab2d060c27315aa4c1017947289c50e25
Jerry Marollano <70122455+chrmux@users.noreply.github.com>
2021-10-05 13:28:57 +0800

    Update director.py

--- mastermind/game/director.py
+++ mastermind/game/director.py
@@ -3,13 +3,10 @@ import random
 
 
 class Director:
-    """A code template for a person who directs the game. The responsibility of 
-    this class of objects is to control the sequence of play.Stereotype:
+    """Sets up the board with an entry for each player.
         
-        Controller
-
-    Attributes:
-        code (Code): An instance of the class of objects known as Code.
+        Args:
+            self (Board): an instance of Board.
     """
     def __init__(self):
         """The class constructor.
@@ -17,25 +14,58 @@ class Director:
         Args:
             self (Director): an instance of Director.
         """
-        self.code = Code()
+        
         self.current = -1
         self.players = []
-        self.name = []
-        self.guess = []
-        self.hint = []
+        self._code = Code()
 
 
     def get_code(self, player_name):
-        for n in range(2):
-            name = input(f"Enter a name for player {n + 1}: ")
-            player = name                                 
-            if player not in self.players:               
-                self.players.append(player_name)
+        """Sets up the board with an entry for each player.
+        
+        Args:
+            self (Board): an instance of Board.
+        """
+        player1 = str(input("Enter a name for player 1: "))
+        player2 = str(input("Enter a name for player 2: "))
+        guess = "----"
+        hint = "****"
+        hint = self._code.generate_code(guess, hint)
+        # print out the player names
+        print("-------------------------------\n")
+        print(f"Player {player1} : ----, ****")
+        print(f"Player {player2} : ----, ****")
+        print("-------------------------------\n")
+        #player one
+        print(f"{player1}'s turn:")
+        player_1 = int(input("What is your guess? "))
+        print()
+        print("-------------------------------\n")
+        print(f"Player {player1} : {player_1}, {hint}")
+        print(f"Player {player2} : {guess}, {hint}")
+        print("-------------------------------\n")
+        # player two
+        print(f"{player2}'s turn:")
+        player_2 = int(input("What is your guess? "))
+        print()
+        print("-------------------------------\n")
+        print(f"Player {player1} : {player_1}, {hint}")
+        print(f"Player {player2} : {player_2}, {hint}")
+        print("-------------------------------\n")
+        
+        
+        code = str(random.randint(1000, 10000))
+        player_name = code
+        if player_1 == code:
+            print(f"{player_1} Won!")
+        elif player_2 == code:
+            print(f"{player_2} Won!")
+        else:
 
-            player_name = (self.current + 1) % len(self.players)
-        return player_name
+            print("\n\nGreat, you got it in", player_name,  "guesses!")
+            
 
-players = Director()
-players.get_code(players)
-code = Code()
-print(code.generate_code(players))
+    def start_game(self):
+        "Start the game loop"       
+        players = Director()
+        players.get_code(players)
